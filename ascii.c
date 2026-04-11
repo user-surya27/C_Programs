c759e029f30ddd883f027b624edb93b11a6cb2c9
Surya Velliyangiri <tyzusurya@gmail.com>
2026-04-11 10:01:28 +0530

    Added ASCII Program

--- ascii.c
+++ ascii.c
@@ -1,12 +1,9 @@
+// C Program for finding the ASCII value 
 #include <stdio.h>
 int main() {  
     char c;
     printf("Enter a character: ");
     scanf("%c", &c);  
-    
-    // %d displays the integer value of a character
-    // %c displays the actual character
     printf("ASCII value of %c = %d", c, c);
-    
     return 0;
 }
