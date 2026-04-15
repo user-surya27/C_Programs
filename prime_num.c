a5a34ddc89cd47ad9df84c1676bfe3b26d381b00
Surya Velliyangiri <tyzusurya@gmail.com>
2026-04-15 20:55:12 +0530

    Updated Code
    Implement a C program to find and print prime numbers up to N.

--- prime_num.c
+++ prime_num.c
@@ -1,1 +1,24 @@
 // This file is for the C program which finds the prime number for N numbers
+#include <stdbool.h>
+#include <stdio.h>
+#include <math.h>
+bool isPrime(int n)
+{
+    if (n == 1 || n == 0)
+        return false;
+    for (int i = 2; i <= sqrt(n); i++) {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+int main()
+{
+    int N = 50;
+    for (int i = 1; i <= N; i++) {
+        if (isPrime(i)) {
+            printf("%d ", i);
+        }
+    }
+    return 0;
+}
